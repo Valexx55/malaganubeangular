9b9db967eccfd32b45f2aefdaf45a39ecc5cf817
valexx55 <valexx55@gmail.com>
2023-07-12 13:40:08 +0200

    form 2/2 post

--- my-app/src/app/components/formulario-restaurante/formulario-restaurante.component.ts
+++ my-app/src/app/components/formulario-restaurante/formulario-restaurante.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { Restaurante } from 'src/app/models/restaurante';
+import { RestauranteService } from 'src/app/services/restaurante.service';
 
 @Component({
   selector: 'app-formulario-restaurante',
@@ -12,8 +13,31 @@ export class FormularioRestauranteComponent implements OnInit {
   //está conectado al formulario
   restaurante:Restaurante;
 
-  constructor() {
+  barrios:Array<String>
+
+  constructor(private restauranteService : RestauranteService) {
     this.restaurante = new Restaurante();
+
+    this.restaurante.nombre="El Cateto";
+    this.restaurante.direccion="La fuente s/n";
+    this.restaurante.barrio="Este";
+    this.restaurante.web="http://www.elcateto.org";
+    this.restaurante.fichaGoogle="http://www.google.elcateto.org";
+    this.restaurante.latitud=30;
+    this.restaurante.longuitud=30;
+    this.restaurante.precio=10;
+    this.restaurante.especialidad1="Marisco";
+    this.restaurante.especialidad2="Pescado";
+    this.restaurante.especialidad3="Carne";
+ 
+
+
+    this.barrios = [
+      '---','Centro', 'Este', 'Ciudad Jardín', 'Bailén-Miraflores', 
+      'Palma-Palmilla', 'Cruz de Humilladero', 'Carretera de Cádiz', 
+      'Churriana', 'Campanillas', 'Puerto de la Torre',
+      'Teatinos-Universidad'
+    ];
    }
 
   ngOnInit(): void {
@@ -23,6 +47,34 @@ export class FormularioRestauranteComponent implements OnInit {
   crearRestaurante()
   {
     console.log("enviar los datos");
+    console.log(`Restaurante 
+     ${this.restaurante.nombre}
+     ${this.restaurante.direccion}
+     ${this.restaurante.barrio}
+     ${this.restaurante.web}
+     ${this.restaurante.fichaGoogle}
+     ${this.restaurante.latitud}
+     ${this.restaurante.longuitud}
+     ${this.restaurante.precio}
+     ${this.restaurante.especialidad1}
+     ${this.restaurante.especialidad2}
+     ${this.restaurante.especialidad3}
+    `);
+
+    this.restauranteService.postRestaurante(this.restaurante).subscribe(
+      {
+        complete: ()=> console.log(`com completa`),
+        error: errorRx => {
+          console.error(errorRx);
+          alert(`Error al insertar el restaurante`);
+
+      },
+      next: restauranteNuevo => {
+        alert(`Restaurante insertado correctamente con id ${restauranteNuevo.id}`);
+      }
+    }
+    );
+
   }
 
 }
--- my-app/src/app/services/restaurante.service.ts
+++ my-app/src/app/services/restaurante.service.ts
@@ -1,4 +1,4 @@
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { Restaurante } from '../models/restaurante';
@@ -10,11 +10,18 @@ export class RestauranteService {
 
   static readonly URL_GET_RESTAURANTES:string = "http://localhost:8081/restaurante";
 
+  cabeceras : HttpHeaders = new HttpHeaders({'Content-type': 'application/json'});
+
   constructor(private http:HttpClient) { }
 
   getListaRestaurantes ():Observable<Array<Restaurante>>
   {
       return this.http.get<Array<Restaurante>>(RestauranteService.URL_GET_RESTAURANTES);
+  }
+
+  postRestaurante (restaurante:Restaurante):Observable<Restaurante>
+  {
+    return this.http.post<Restaurante>(RestauranteService.URL_GET_RESTAURANTES, restaurante, {headers:this.cabeceras});
   }
 
 }
